a65c9caafdecab857575e24fe9c0d1b495e53248
Sajid <yeamimhossainsajid@gmail.com>
2025-09-23 11:56:22 +0600

    first commit

---
+++ src/main/java/com/sajid/Achievia/AchieviaApplication.java
@@ -0,0 +1,13 @@
+package com.sajid.Achievia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchieviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchieviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sajid/Achievia/AchieviaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajid.Achievia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchieviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
