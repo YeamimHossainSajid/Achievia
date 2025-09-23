b7096152f6c11838f262ede1eb371e7a497fc0b0
Sajid <yeamimhossainsajid@gmail.com>
2025-09-23 12:16:18 +0600

    Auth almost done

---
+++ src/main/java/com/sajid/Achievia/config/notification/SSEService.java
@@ -0,0 +1,17 @@
+package com.sajid.Achievia.config.notification;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+@Service
+public class SSEService<T> {
+    private final Sinks.Many<T> sink = Sinks.many().multicast().onBackpressureBuffer();
+
+    public Flux<T> subscribe() {
+        return sink.asFlux();
+    }
+
+    public void emit(T data) {
+        sink.tryEmitNext(data);
+    }
+}
