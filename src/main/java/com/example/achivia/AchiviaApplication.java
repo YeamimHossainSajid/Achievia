8b65def3e46784f78e90e2b2325818182d2f05f5
Sajid <yeamimhossainsajid@gmail.com>
2025-10-07 21:28:57 +0600

    ..

---
+++ src/main/java/com/example/achivia/AchiviaApplication.java
@@ -0,0 +1,13 @@
+package com.example.achivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchiviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchiviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/achivia/AchiviaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.achivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchiviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
