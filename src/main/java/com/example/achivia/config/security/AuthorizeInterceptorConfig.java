742b2cee15e0318bfb86b660aee95a8e4748dd76
Sajid <yeamimhossainsajid@gmail.com>
2025-10-20 00:00:26 +0600

    deleted the images

--- src/main/java/com/example/achivia/config/security/AuthorizeInterceptorConfig.java
+++ src/main/java/com/example/achivia/config/security/AuthorizeInterceptorConfig.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+
 @Component
 public class AuthorizeInterceptorConfig implements HandlerInterceptor {
 
